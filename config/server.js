7f1e021c021b671cf6d93da36febafff510af6cf
Curt Croatt <cjc83486@gmail.com>
2021-01-29 13:31:45 -0600

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b8c1cfc0db483419d19784bf897c0370'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f7b64ceb-d111-481b-9bbe-fa67f55cc288'
+};
